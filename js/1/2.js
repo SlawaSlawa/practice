2bd08fbf37b1c8c81501a579f1c4b3afa7652a73
Slawa <slawapocht4@yandex.ru>
2021-04-13 22:38:08 +0400

    refactoring 2.js

--- js/1/2.js
+++ js/1/2.js
@@ -1,26 +1,28 @@
 'use strict';
 const year1 = prompt('Введи с какого года начать');
 const year2 = prompt('Введи до какого года');
+let yearStart = 0;
+let yearEnd = 0;
 
-if (year1 <= year2) {
-	for (let year = year1 ; year <= year2; year++) {
-		if ((year % 100 == 0) && (year % 400 != 0)) {
-		    console.log(year + " Обычный");
-		} else if (year % 4 == 0) {
-		    console.log(year + " Високосный");
-		} else {
-		    console.log(year + " Обычный");
-		}	
-	}
-}else {
-	for (let year = year2 ; year <= year1; year++) {
-		if ((year % 100 == 0) && (year % 400 != 0)) {
-		    console.log(year + " Обычный");
-		} else if (year % 4 == 0) {
-		    console.log(year + " Високосный");
-		} else {
-		    console.log(year + " Обычный");
-		}	
-	}
+if (year1 === '' || year2 === '' || isNaN(+year1) || isNaN(+year2)) {
+    alert('Введены не правильные данные');
+} else {
+    if (+year1 <= +year2) {
+        yearStart = +year1;
+        yearEnd = +year2;
+    } else {
+        yearStart = +year2;
+        yearEnd = +year1;
+    }
+
+    for (let year = yearStart; year <= yearEnd; year++) {
+        if ((year % 100 == 0) && (year % 400 != 0)) {
+            console.log(year + " Обычный");
+        } else if (year % 4 == 0) {
+            console.log(year + " Високосный");
+        } else {
+            console.log(year + " Обычный");
+        }
+    }
+
 }
-
