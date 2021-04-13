b336b964b2a878e5f4020a13171b89523885871a
Slawa <slawapocht4@yandex.ru>
2021-04-13 22:58:00 +0400

    added check to null in 1.js

--- js/1/1.js
+++ js/1/1.js
@@ -1,26 +1,30 @@
 'use strict';
 
 let num1 = prompt('Введи первое число: ');
-let num2 = prompt('Введи второе число: ');
 
+if (num1 !== null) {
+    let num2 = prompt('Введи второе число: ');
 
-if (num1 === '' || num2 === '') {
-    alert('Введены не правильные данные');
-} else {
-    num1 = +num1;
-    num2 = +num2;
+    if (num2 !== null) {
+        if (num1 === '' || num2 === '') {
+            alert('Введены не правильные данные');
+        } else {
+            num1 = +num1;
+            num2 = +num2;
 
-    if (isNaN(num1) || isNaN(num2)) {
-    	alert('Введены не правильные данные');
-    }else {
-    	if (num1 > num2) {
-    		alert('Первое число больше второго');
-    	}
-    	if (num1 < num2) {
-    		alert('Второе число больше первого');
-    	}
-    	if (num1 === num2) {
-    		alert('Числа равны');
-    	}
+            if (isNaN(num1) || isNaN(num2)) {
+                alert('Введены не правильные данные');
+            } else {
+                if (num1 > num2) {
+                    alert('Первое число больше второго');
+                }
+                if (num1 < num2) {
+                    alert('Второе число больше первого');
+                }
+                if (num1 === num2) {
+                    alert('Числа равны');
+                }
+            }
+        }
     }
 }
