187ed1c31eefb40fa7a0fcbb74c2687d33c3fbaa
Slawa <slawapocht4@yandex.ru>
2021-04-13 21:25:01 +0400

    solving the first test

--- js/1/1.js
+++ js/1/1.js
@@ -1,3 +1,26 @@
 'use strict';
 
-alert(1);
+let num1 = prompt('Введи первое число: ');
+let num2 = prompt('Введи второе число: ');
+
+
+if (num1 === '' || num2 === '') {
+    alert('Введены не правильные данные');
+} else {
+    num1 = +num1;
+    num2 = +num2;
+
+    if (isNaN(num1) || isNaN(num2)) {
+    	alert('Введены не правильные данные');
+    }else {
+    	if (num1 > num2) {
+    		alert('Первое число больше второго');
+    	}
+    	if (num1 < num2) {
+    		alert('Второе число больше первого');
+    	}
+    	if (num1 === num2) {
+    		alert('Числа равны');
+    	}
+    }
+}
